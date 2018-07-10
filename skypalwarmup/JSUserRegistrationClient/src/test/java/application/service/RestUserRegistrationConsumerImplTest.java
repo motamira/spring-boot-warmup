08d08244b430ac92bda9c44b14c2589d271bf7a1
Karim Farid <karim.farid@jumia.com>
2018-07-10 13:57:59 +0200

    create JSUserRegistrationClient test

---
+++ skypalwarmup/JSUserRegistrationClient/src/test/java/application/service/RestUserRegistrationConsumerImplTest.java
@@ -0,0 +1,15 @@
+package application.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Copyright (c) 2016-2018, Jumia.
+ */
+public class RestUserRegistrationConsumerImplTest {
+
+    @Test
+    public void postUserRegistration() {
+    }
+}
